347395e06bc8e608fa150da9cefbba4e8b9608d0
Ahmed Alhashimi <alhashimi346@gmail.com>
2019-10-29 14:20:54 -0600

    use themed look using themeprovider

--- Slick-fits/frontend/components/Header.js
+++ Slick-fits/frontend/components/Header.js
@@ -1,9 +1,32 @@
 import Nav from './Nav';
+import Link from 'next/link';
+import styled from 'styled-components';
 
+const Logo = styled.h1`
+  font-size: 2rem;
+  margin-left: 2rem;
+  position: relative;
+  z-index: 2;
+  transform: skew(-7deg);
+
+  a {
+    padding: 0.5rem 1rem;
+    background: ${props => props.theme.black};
+    color: white;
+    text-transform: uppercase;
+    text-decoration: none;
+  }
+`;
 const Header = () => (
   <div>
     <div className="bar">
-      <a href="">Slick-Fits</a>
+      <h1>
+        <Logo>
+          <Link href="/">
+            <a>Slick Moda</a>
+          </Link>
+        </Logo>
+      </h1>
       <nav>
         <Nav />
       </nav>

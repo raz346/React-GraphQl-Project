a76afa83fb1fd740a9aee8db9948f494b572f5ca
Ahmed Alhashimi <alhashimi346@gmail.com>
2019-11-22 14:03:15 -0700

    add more tabs to navbar and style them

--- Slick-fits/frontend/components/Nav.js
+++ Slick-fits/frontend/components/Nav.js
@@ -3,11 +3,30 @@ import NavStyles from './styles/NavStyles';
 
 const Nav = () => (
   <NavStyles>
-    <Link href="/sell">
-      <a>sell</a>
-    </Link>
+
     <Link href="/">
       <a>Home</a>
+    </Link>
+   
+    <Link href="/sell">
+      <a>Sell</a>
+    </Link>
+   
+    <Link href="/items">
+    <a>Items</a>
+    </Link>
+
+
+    <Link href="/orders">
+      <a>Orders</a>
+    </Link>
+
+    <Link href="/me">
+      <a>Account</a>
+    </Link>
+
+    <Link href="/signup">
+      <a>Signup</a>
     </Link>
   </NavStyles>
 );
